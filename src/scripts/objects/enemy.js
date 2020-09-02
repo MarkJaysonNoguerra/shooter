9d3cb3695e69353bf4a725432a6fcf15c5f38cc5
unknown <noguerra9999@gmail.com>
2020-09-02 03:32:25 -0700

    generate enemy with random movements(this needs some improvement)

--- src/scripts/objects/enemy.js
+++ src/scripts/objects/enemy.js
@@ -4,10 +4,24 @@ export default class Enemy extends Phaser.Physics.Arcade.Sprite {
   constructor(scene, x, y) {
     super(scene, x, y, 'enemy')
     scene.add.existing(this)
+    // scene.add.image(0, 80, 'enemy', 0);
 
+    scene.tweens.add({
+      targets: this,
+      props: {
+          x: { value: 1000, duration: 4000, flipX: true },
+          y: { value: 800, duration: 8000,  },
+      },
+      ease: 'Sine.easeInOut',
+      yoyo: true,
+      repeat: -1
+  });
+
+    
   }
   update() {
     this.setInteractive()
-        .on('pointerdown', () => console.log("enemy shoot!"))
+        .on('pointerdown', () => this.setVisible(false))
   } 
+
 }
--- src/scripts/scenes/mainScene.js
+++ src/scripts/scenes/mainScene.js
@@ -12,19 +12,27 @@ export default class MainScene extends Phaser.Scene {
   }
    
   create() {
-    /**
-     * Delete all the code below to start a fresh scene
-     */
+
     this.add.image(1280 / 2, 720 / 2, 'bg');
     this.fpsText = new FpsText(this)
     this.enemies = new Enemy(this, this.cameras.main.width / 2, this.cameras.main.displayHeight /2);
+    this.enemies2 = new Enemy(this, this.cameras.main.width / 2.3, this.cameras.main.displayHeight /2);
+    this.enemies3 = new Enemy(this, this.cameras.main.width / 1, this.cameras.main.displayHeight /2);
+    this.enemies4 = new Enemy(this, this.cameras.main.width / 3, this.cameras.main.displayHeight /2);
+    this.enemies5 = new Enemy(this, this.cameras.main.width / 6, this.cameras.main.displayHeight /2);
+
     this.enemies.update()
+    this.enemies2.update()
+    this.enemies3.update()
+    this.enemies4.update()
+    this.enemies5.update()
     this.crosshair = new Crosshair(this, this.cameras.main.width / 2, this.cameras.main.displayHeight /2)
+    
   }
 
   update() {
     this.fpsText.update()
-    // this.enemies.update()
+
   }
   
 }
